cf4a3c162f9c280d01771e641a4c9590b5c418aa
Jelena Nemčić <jelena.nemcic@fer.hr>
2022-03-13 10:50:31 +0100

    added UI screens

---
+++ GibalicaG2/Assets/Gibalica/Scripts/UIManager.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class UIManager : MonoBehaviour
+{
+
+  public void StartGibalica()
+  {
+    SceneManager.LoadScene("ExerciseScene");
+  }
+
+  public void LoadAboutUs()
+  {
+    SceneManager.LoadScene("AboutScene");
+  }
+
+  public void LoadWelcome()
+  {
+    SceneManager.LoadScene("WelcomeScene");
+  }
+}
